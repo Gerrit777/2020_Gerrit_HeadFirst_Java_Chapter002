64e0a73faebf6b43bcd40b41cf463640609e2d67
Gerrit Brands <g.brands1@chello.nl>
2020-05-12 17:40:56 +0200

    Werkende versies !

---
+++ src/Certificates.java
@@ -0,0 +1,32 @@
+class StudentResults{
+    private String Full_Name;
+
+    StudentResults() {
+        Full_Name = "No Name Given";
+    }
+    String fullName(String aName) {
+        if (aName.equals("Bill Gates")) { aName="BG";}
+        if (aName.equals("Bill Clinton")) {aName="BC";}
+        Full_Name = aName;
+    return Full_Name;
+    }
+}
+
+public class Certificates extends StudentResults {
+    private String certificate;
+
+    Certificates(){
+        super();
+        certificate = "No Certificate Awarded";
+    }
+}
+
+
+class ExamDetails{
+    public static void main(String[] args){
+       StudentResults aStudent = new StudentResults();
+       String sName = aStudent.fullName("Bill Gates");
+
+       System.out.println(sName);
+    }
+}
