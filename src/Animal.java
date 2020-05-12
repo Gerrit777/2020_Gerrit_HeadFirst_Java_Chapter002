f6a75f7fd80f61d36e1826feb05a16e16aad2e89
Gerrit Brands <g.brands1@chello.nl>
2020-05-12 16:07:59 +0200

    Werkende versies !

---
+++ src/Animal.java
@@ -0,0 +1,21 @@
+class Dier{
+    int age;
+}
+
+class Mammal extends Dier{
+    String furColor;
+    void liveBirth() {System.out.println("Baby baaren"); }
+}
+
+class Human extends Mammal{
+    String languageSpoken;
+    void speak(){System.out.println("Praten"); }
+}
+
+class DierMamalHuman {
+    public static void main(String[] args) {
+        Human elvis=new Human();
+        elvis.liveBirth();
+        elvis.speak();
+    }
+}
